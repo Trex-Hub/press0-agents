d8d136b2b4c66ae789f10ff94e04ada4afba80ec
Devendra Mishra <devendra@mavonic.com>
2025-11-08 15:49:21 +0530

    chore(cleanup): removed perplexica search

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,25 +8,6 @@ export const LOGGER_LEVEL = process.env.LOGGER_LEVEL || "info";
 export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY!;
 export const GOOGLE_MODEL = process.env.GOOGLE_MODEL || "gemini-2.0-flash";
 
-// PERPLEXICA
-
-export const PERPLEXICA_BASE_URL =
-  process.env.PERPLEXICA_BASE_URL || "http://localhost:3000";
-
-export const PERPLEXICA_CONFIG = {
-  chatModel: {
-    provider: "openai",
-    name: "gpt-4o-mini",
-  },
-  embeddingModel: {
-    provider: "openai",
-    name: "text-embedding-3-large",
-  },
-  optimizationMode: "speed",
-  focusMode: "webSearch",
-  stream: false,
-};
-
 // DATABASE
 export const DATABASE_URL = process.env.DATABASE_URL!; 
 
